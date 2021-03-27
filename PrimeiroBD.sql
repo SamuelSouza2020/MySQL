3e3c989b2837ba45020ea03f84be0eff7121d955
Samuel Souza <souzasamuel623@gmail.com>
2021-03-27 16:09:53 -0300

    Primeiro BD

--- PrimeiroBD.sql
+++ PrimeiroBD.sql
@@ -1,7 +1,12 @@
 CREATE DATABASE cadastro;
 
+USE cadastro;
+
 CREATE TABLE pessoas(
-
-
-
+nome VARCHAR(30),
+idade TINYINT,
+sexo CHAR(1),
+peso FLOAT,
+altura FLOAT,
+macionalidade VARCHAR(20)
 );
