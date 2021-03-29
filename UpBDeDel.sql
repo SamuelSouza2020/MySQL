ae04d4b8f832080d6d896144fe44aebaa7a8c964
Samuel Souza <souzasamuel623@gmail.com>
2021-03-29 14:53:36 -0300

    Update UpBDeDel.sql

--- UpBDeDel.sql
+++ UpBDeDel.sql
@@ -14,3 +14,30 @@ peso DECIMAL (5,2),
 altura DECIMAL (3,2), 
 nacionalidade VARCHAR (20) DEFAULT 'Brasil'
 )DEFAULT CHARSET = utf8;
+
+USE cadastro;
+
+INSERT INTO pessoas
+(nome, nascimento, sexo, peso, altura, nacionalidade)
+values
+('Nicole', '2000-08-05', 'F', '76.8', '1.7', 'Brasil');
+
+SELECT * FROM pessoas;
+
+INSERT INTO pessoas VALUE
+(DEFAULT, 'Carlos', '1982-09-26', 'M', '83.7', '1.64', 'Canadá'),
+(DEFAULT, 'Simon', '2010-03-01', 'M', '5.87', '0.60', 'Chile'),
+(DEFAULT, 'Donald', '1920-06-17', 'M', '81.7', '1.87', DEFAULT);
+
+SELECT * FROM pessoas;
+
+desc pessoas;
+alter table pessoas
+add column profissao varchar(10);
+
+desc pessoas;
+
+alter table pessoas
+drop column profissao;
+
+select * from pessoas;
