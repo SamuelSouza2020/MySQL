2364e542faea0527b5355c910c88e97c75e539f0
Samuel Souza <61950221+SamuelSouza2020@users.noreply.github.com>
2021-03-29 10:55:23 -0300

    SQL Estruturado

--- UpBDeDel.sql
+++ UpBDeDel.sql
@@ -7,10 +7,12 @@ DEFAULT COLLATE utf8_general_ci;
 USE cadastro;
 
 CREATE TABLE pessoas (
+id INT NOT NULL AUTO_INCREMENT,
 nome varchar(30) NOT NULL,
 nascimento DATE,
 sexo ENUM ('M', 'F'),
 peso DECIMAL (5,2),
 altura DECIMAL (3,2), 
-nacionalidade VARCHAR (20) DEFAULT 'Brasil'
+nacionalidade VARCHAR (20) DEFAULT 'Brasil',
+PRIMARY KEY (id)
 )DEFAULT CHARSET = utf8;
